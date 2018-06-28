6db29b6c3d71ae5738090f669a04020dbc37e749
Rosa Tu <rosalina.tu@gmail.com>
2018-06-28 01:58:24 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,35 @@
+
+    def instantiate_new_array
+        newArray = []
+        return newArray
+      end
+
+     def array_with_two_elements
+        twoArray = [1,2]
+        return twoArray
+      end 
+      
+     def first_element(array)
+      return array[0]
+    end
+  
+
+     def third_element(array)
+       return array[2]
+    end
+
+    def last_element(array)
+      return array[-1]
+  end
+
+    def first_element_with_array_methods(array) 
+      return array.first
+    end
+
+    def last_element_with_array_methods(array)
+      return array.last
+    end
+
+  def length_of_array(array)
+      return array.length
+    end
